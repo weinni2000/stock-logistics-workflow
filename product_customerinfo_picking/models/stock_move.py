78b8900d1d483df207587a9b17709b3dcad0cd8c
Nik <weinni2000@gmail.com>
2026-01-20 09:03:34 +0100

    18-product_customerinfo_picking-partner-selection

--- product_customerinfo_picking/models/stock_move.py
+++ product_customerinfo_picking/models/stock_move.py
@@ -6,6 +6,13 @@ from odoo import api, fields, models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
+    def process_template(self, m, mo):
+        if mo.picking_id.partner_id.parent_id:
+            partner_id = mo.picking_id.partner_id.parent_id
+        else:
+            partner_id = mo.picking_id.partner_id
+        return m.partner_id == partner_id
+
     @api.depends(
         "picking_id.partner_id",
         "product_id",
@@ -23,17 +30,11 @@ class StockMove(models.Model):
             ):
                 customer = fields.first(
                     move.product_tmpl_id.customer_ids.filtered(
-                        lambda m, mo=move: m.partner_id == mo.picking_id.partner_id
+                        # lambda m, mo=move: m.partner_id == partner_id
+                        lambda m, mo=move: self.process_template(m, mo)
                     )
                 )
                 product_customer_code = customer.product_code
                 product_customer_name = customer.product_name
             move.product_customer_code = product_customer_code
             move.product_customer_name = product_customer_name
-
-    product_customer_code = fields.Char(
-        compute="_compute_product_customer_code",
-    )
-    product_customer_name = fields.Char(
-        compute="_compute_product_customer_code",
-    )
